4605f13a8ae55f29f6a35242c3d8d96805d87b78
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-07-09 08:27:59 +0100

    Add a percentage check and make sure no message is sent if not malicious

--- cogs/scan_url.py
+++ cogs/scan_url.py
@@ -13,11 +13,15 @@ class ScanURLCog:
 
     async def on_message(self, message):
         # First, scan the message with the VirusTotal API
+        #TODO: Select all URLs from a message
         url = re.search("(?P<url>https?://[^\s]+)", message.content).group("url") # Find a URL in the message, note that this currently only finds the first URL in the message
         if url != None:
             # If our url is not empty (a url was not found)
             response = vt.get_url_report(url)
-            await message.channel.send('VirusTotal results: ' + str(response['results']['positives']) + ' / ' + str(response['results']['total']) + ' scanners found that this link was malicious.')
+            if int(response['results']['positives']) != 0:
+                # No need to send the message if no maliciousness was found
+                percent_malicious = (int(response['results']['positives']) / int(response['results']['total'])) * 100
+                await message.channel.send('VirusTotal results: ' + str(response['results']['positives']) + ' / ' + str(response['results']['total']) + ' (' + str(percent_malicious) + '%) of scanners found that this link was malicious.')
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case ScanURLCog.
 # When we load the cog, we use the name of the file.
